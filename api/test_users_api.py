f5ab66626eee798cd6a081819f306e3851f7e24c
Shubham <shubham.v.mirani@gmail.com>
2026-04-21 16:24:58 +0530

    Api testing

--- api/test_users_api.py
+++ api/test_users_api.py
@@ -1,14 +1,19 @@
 import json
 
-def test_get_posts(api_context):
+def test_get_posts_with_headers(api_context):
+    headers = {
+        "Accept": "application/json",
+        "Custom-Header": "Playwright-Test"
+    }
+
     response = api_context.get(
-        "https://jsonplaceholder.typicode.com/posts"
+        "https://jsonplaceholder.typicode.com/posts",
+        headers=headers
     )
 
     assert response.status == 200
 
     body = response.json()
 
-    # 🔹 Print JSON nicely in terminal
     print("\nAPI RESPONSE JSON:")
     print(json.dumps(body, indent=2))
